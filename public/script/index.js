ce728cd2cd4a6a2a8c1871e1000434499a792c40
Kirubel Eshetu Tefera <Kirubelwinner@gmail.com>
2025-12-31 22:04:49 +0300

    split html from script

--- public/script/index.js
+++ public/script/index.js
@@ -1,14 +1,25 @@
 console.log("JZPIS coming up!");
 
-document.addEventListener('DOMContentLoaded', function() {
-    const links = document.querySelectorAll('.link');
-  
-    links.forEach(function(link) {
-      link.addEventListener('click', function() {
-        links.forEach(function(l) {
-          l.classList.remove('clicked');
-        });
-        this.classList.add('clicked');
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll(".link");
+
+  links.forEach(function (link) {
+    link.addEventListener("click", function () {
+      links.forEach(function (l) {
+        l.classList.remove("clicked");
       });
+      this.classList.add("clicked");
     });
   });
+});
+
+function updateTime() {
+  var now = new Date();
+  var date = now.toDateString();
+  var time = now.toLocaleTimeString();
+  document.getElementById("datetime").innerHTML =
+    "Date: " + date + "<br>Time: " + time;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
